27ed8191515190c486299387bc66e730f86dcef8
Yami-no-karuro <carloborgna98@gmail.com>
2025-04-02 19:33:02 +0200

    Basic str_replace

---
+++ readme.md
@@ -0,0 +1,18 @@
+# C - Skeleton
+
+## Starter template for C applications
+
+### Setup
+
+- Clone the repository, `` git clone https://github.com/Yami-no-karuro/C-Skeleton.git ``
+- Change directory to the skeleton project, `` cd C-Skeleton ``
+- Remove the existing repository, `` rm -rf .git ``
+- Make the console utility file executable, `` chmod +x ./bin/console ``
+- Run the requirements check, `` bin/console check-requirements ``
+- Enjoy!
+
+### Commands
+
+- `` bin/console check-requirements ``
+- `` bin/console build <debug|release> <source> ``
+- `` bin/console run <source> ``
---
+++ src/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *str_replace(const char *subject, const char *search, const char *replace)
+{
+    char *offset = strstr(subject, search);
+    if (offset == NULL)
+        return NULL;
+
+    int offset_index = offset - subject;
+    int search_length = strlen(search);
+    int replace_length = strlen(replace);
+    int result_length = strlen(subject) - search_length + replace_length;
+
+    char *result = (char *)malloc((result_length + 1) * sizeof(char));
+    if (!result)
+        return NULL;
+
+    strncpy(result, subject, offset_index);
+    strcpy(result + offset_index, replace);
+    strcpy(result + offset_index + replace_length, subject + offset_index + search_length);
+
+    return result;
+}
+
+int main() {
+    char *input = "Hello, my name is Carletto and i am a software developer!";
+    char *output = str_replace(input, "Carletto", "Yami-no-karuro");
+
+    printf("Input: %s\n", input);
+    printf("Output: %s\n", output);
+    free(output);
+
+    return 0;
+}
