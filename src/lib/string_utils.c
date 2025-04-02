5a2708ece049fd36395e0afa8d3d310411513b28
Yami-no-karuro <carloborgna98@gmail.com>
2025-04-02 21:23:54 +0200

    String utils lib

---
+++ src/lib/string_utils.c
@@ -0,0 +1,57 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "string_utils.h"
+
+/**
+ * Replaces the first occurrence of a substring within a string with another substring.
+ *
+ * @param subject - The original string in which the replacement is performed.
+ * @param search - The substring to be replaced.
+ * @param replace - The substring to replace the search string.
+ * @return - The resulting string.
+ */
+char *str_replace(const char *subject, const char *search, const char *replace)
+{
+    char *offset = strstr(subject, search);
+    if (offset == NULL)
+        return NULL;
+
+    int offset_index = offset - subject;
+    int search_length = strlen(search);
+    int replace_length = strlen(replace);
+    int result_length = strlen(subject) - search_length + replace_length;
+
+    char *result = (char *)malloc((result_length + 1) * sizeof(char));
+    if (!result)
+        return NULL;
+
+    strncpy(result, subject, offset_index);
+    strcpy(result + offset_index, replace);
+    strcpy(result + offset_index + replace_length, subject + offset_index + search_length);
+
+    return result;
+}
+
+/**
+ * Replaces all occurrences of a substring within a string with another substring.
+ *
+ * @param subject - The original string in which the replacements are performed.
+ * @param search - The substring to be replaced.
+ * @param replace - The substring to replace the search string.
+ * @return - The resulting string with all occurrences replaced.
+ */
+char *str_replace_all(const char *subject, const char *search, const char *replace)
+{
+    char *result = str_replace(subject, search, replace);
+    if (!result)
+        return NULL;
+    
+    char *next_result;
+    while ((next_result = str_replace(result, search, replace)) != NULL) {
+        free(result);
+        result = next_result;
+    }
+
+    return result;
+}
---
+++ src/lib/string_utils.h
@@ -0,0 +1,7 @@
+#ifndef STRING_UTILS_H
+#define STRING_UTILS_H
+
+char *str_replace(const char *subject, const char *search, const char *replace);
+char *str_replace_all(const char *subject, const char *search, const char *replace);
+
+#endif
